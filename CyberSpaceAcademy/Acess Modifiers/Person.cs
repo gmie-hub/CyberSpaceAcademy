a48bd104c58bbabc0244204cdfba0c11e1fb44f0
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-05 04:11:20 -0700

    Acess modifier tuturial

---
+++ CyberSpaceAcademy/Acess Modifiers/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSpaceAcademy.Acess_Modifiers
+{
+    public class Person
+    {
+
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        } 
+    }
+}
--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -2,18 +2,37 @@
 
 namespace CyberSpaceAcademy
 {
+    public class Person
+    {
+
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
         {
+         
+            //var customer = new Customer(1, "John");
 
-            var customer = new Customer(1, "John");
+            //var order = new Order();
+            //customer.orders.Add(order);
 
-            var order = new Order();
-            customer.orders.Add(order);
+            //Console.WriteLine(customer.Id);
+            //Console.WriteLine(customer.Name);
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1999, 09, 02));
+            Console.WriteLine(person.GetBirthdate());
             
           
         }
