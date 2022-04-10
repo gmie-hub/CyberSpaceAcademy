cd13ea1de2b74fd6ab83e780b57be13a53dfbcb7
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-10 05:35:41 -0700

    this is an example on class

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -2,47 +2,54 @@
 
 namespace CyberSpaceAcademy
 {
+   public class Dog
+    {
+        string name;
+        string breed;
+        int age;
+        string color;
 
+        public Dog( string name, string breed, int age, string color)
+        {
+
+            this.name = name;
+            this.breed = breed;
+            this.age = age;
+            this.color = color;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetBreed()
+        {
+            return breed;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        public string ToString()
+        {
+            return ("Hi my name is " + this.GetName() + ".\nMy breed, age and color are " +
+                this.GetBreed() + ", " + this.GetAge() + " and " + this.GetColor());
+        }
+    }
     public class Program
     {
         public static void Main()
         {
-
-            string topic;
-            string category;
-
-            topic = "Inheritance";
-
-            switch (topic)
-            {
-                case "Introduction to c#":
-                case "Variables":
-                case "Data types":
-
-                    category = "Basics";
-                    break;
-
-                case "Loops":
-                case "If statements":
-                case "Jump statements":
-
-                category = "Control flow";
-                    break ;
-
-                case "Class & Objects":
-                case "Inheritance":
-                case "Constructors":
-
-                    category = "OOPs Concepts";
-                    break;
-
-                default:
-                    category = "Not mentioned";
-                    break;
-            }
-            Console.WriteLine("Category is " + category);
+            Dog tuffy = new Dog("tuffy", "papillion", 5, "white");
+            Console.WriteLine(tuffy.ToString());
         }
     }
-
-   
 }
