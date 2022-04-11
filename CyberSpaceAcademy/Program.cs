c9cad173ab653a2337f8e8fcb676b33a03ec6365
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-11 06:51:51 -0700

    This is the classwork answer

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -2,54 +2,46 @@
 
 namespace CyberSpaceAcademy
 {
-   public class Dog
-    {
-        string name;
-        string breed;
-        int age;
-        string color;
-
-        public Dog( string name, string breed, int age, string color)
-        {
-
-            this.name = name;
-            this.breed = breed;
-            this.age = age;
-            this.color = color;
-        }
-
-        public string GetName()
-        {
-            return name;
-        }
-
-        public string GetBreed()
-        {
-            return breed;
-        }
-
-        public int GetAge()
-        {
-            return age;
-        }
-
-        public string GetColor()
-        {
-            return color;
-        }
-
-        public string ToString()
-        {
-            return ("Hi my name is " + this.GetName() + ".\nMy breed, age and color are " +
-                this.GetBreed() + ", " + this.GetAge() + " and " + this.GetColor());
-        }
-    }
+   
     public class Program
     {
         public static void Main()
         {
-            Dog tuffy = new Dog("tuffy", "papillion", 5, "white");
-            Console.WriteLine(tuffy.ToString());
+            ShoppingBag<Bag> shoppingBag = new ShoppingBag<Bag>();
+            shoppingBag.AddItem(new Bag());
+            var counter = 0;
+          
+        }
+    }
+
+    public class ShoppingBag<T> where T : Bag
+    {
+        private T[] _items = new T[10];
+        private int _index = 0;
+        public void AddItem(T item)
+        {
+
+            _items[_index++] = item;
+        }
+        public T Item { get; set; }
+
+        private T item;
+        public T GetItem(int index)
+        {
+            return item;
+        }
+        public void SetItem(T Bag)
+        {
+            item = Bag;
+        }
+    }
+
+    public class Bag
+    {
+        public Bag()
+        {
+
+           
         }
     }
 }
