122d81004d793f9bd0130ce7bf229c94420ed8d5
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-13 03:54:54 -0700

    Delegate (lambda expression, annonymous delegate method and delegate inheritance

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -2,42 +2,71 @@
 
 namespace CyberSpaceAcademy
 {
+    public delegate int Arithmetic(int first, int second);
+
+    public delegate void ArithmeticDelegate();
+
+    public delegate Shape DrawDelegate();
+
+    public delegate void ShapeDelegate(int i);
+
+    public class Shape
+    {
+
+    }
+
+    public class Triangle : Shape
+    {
+
+    }
     public class Program
     {
 
+        static Triangle DrawTriangle()
+        {
+            return new Triangle();
+        }
         static void Main()
         {
-            People p = new People();
 
-            Person person = p[1];
-            Console.WriteLine(person.Name);
-        }
-    }
-    public class Person
-    {
-
-        public string Name { get; set; }
-    }
-
-    public class People
-    {
-
-        private readonly Person[] names =
-        {
-
-            new Person{Name = "Bill Gates"},
-            new Person{Name = "Prolifik Lexzy"},
-            new Person{Name = "Scott Aniel"},
-            new Person{Name = "Matt Redman"}
-        };
-
-        public Person this[int position]
-        {
-            get
+            ArithmeticDelegate arithmeticDelegate = () =>
             {
-                return names[position];
-            }
+
+            };
+            DrawDelegate drawDelegate = DrawTriangle;
+
+            Arithmetic del = Add;
+            //del += Minus;
+
+            del += delegate (int first, int second)
+            {
+                return first + second;
+            };
+
+            del += (first, second) =>
+            {
+                return second + first;
+            };
+
+            del += (first, second) => second + first;
+
+            
+            var result = del(39, 8);
+            Console.WriteLine(result);
         }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
     }
+
+
 
 }
