2ca25046213a03fe53c897d2629e45a979bc6209
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-16 03:43:35 -0700

    Practice one on this keyword

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -4,67 +4,45 @@ using System;
 namespace CyberSpaceAcademy
 {
 
-    public interface Animal
+    public class User
     {
-        string Name { get; set; }
-        int Age { get; set; }
-        AnimalTypes Type { get; set; }
 
+        public string name, location;
+        public long marks;
 
+        public User(string name, string location)
+        {
+            this.name = name;
+            this.location = location;
+        }
 
-        //public static void Main()
-        //{
-        //    Animal animal = new Animal() { Name = "Goat", Age = 30};
-        //    Console.WriteLine(animal.Name);
-        //    Console.WriteLine(animal.Age);
-        //}
-        //public static void Main()
-        //{
-        //    int[] num = new[] { 1, 2, 3, 4, 5 };
-        //    PrintNumbers(num);
+        public void GetUserDetails()
+        {
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Location: {0}", location);
+            Console.WriteLine("Marks: {0}", Exams.GetPercentage(this));
+        }
+    }
 
-        //}
+    public class Exams
+    {
+        public static double GetPercentage(User u)
+        {
+            double i = ((double)470 / 600) * 100;
+            return (i);
+        }
+    }
 
-        //public static void PrintNumbers(int[] numbers)
-        //{
-        //    for (int i = 0; i < numbers.Length; i++)
-        //    {
-        //        Console.WriteLine(numbers[i]);
-        //    }
-        //}
+    public class program
+    {
 
-        //private string _name;
-        //private int _age;
-        //    public Animal()
-        //    {
-        //        Console.WriteLine("Constructor called");
-        //    }
+        public static void Main()
+        {
 
-        //    public string Name 
-        //    { 
-        //        get 
-        //        { 
-        //            return _name; 
-        //        }
-        //        set
-        //        {
-        //            _name = value;
-        //        }
-        //    }
-
-        //    public int Age
-        //    {
-        //        get
-        //        {
-        //            return _age;
-        //        }
-        //        set 
-        //        { 
-        //            _age = value; 
-        //        }
-        //    }
-        //}
-
-
+            User u = new User("Olaosebikan Oluwafolajimi", "Lagos");
+            u.GetUserDetails();
+            Console.WriteLine("\npress enter key to exit");
+            Console.ReadLine(); 
+        }
     }
 }
