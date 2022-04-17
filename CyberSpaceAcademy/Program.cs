7742af205e7d6e2f118b11c049634904aa97dfb5
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-17 10:12:39 -0700

    Practice one on base keyword

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -4,40 +4,25 @@ using System;
 namespace CyberSpaceAcademy
 {
 
-    public class BClass 
+    public class User
     {
-
-        public virtual string Name { get; set; }
+        public string name = "Jimi";
+        public string location = "Lagos";
+        public int age = 20;
 
         public virtual void GetInfo()
         {
-            Console.WriteLine("Learn C# Tuturial");
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Location: {0}", location);
         }
     }
 
-    public class DClass : BClass
+    public class Details : User
     {
-        private string name;
-
-        public override string Name
-        {
-            get { return name; }
-            set
-            {
-                if (!string.IsNullOrEmpty(value))
-                {
-                    name = value;
-                }
-                else
-                {
-                    name = "No Value";
-                }
-            }
-        }
-
         public override void GetInfo()
         {
-            Console.WriteLine("Welcome To Tutlane");
+            base.GetInfo();
+            Console.WriteLine("Age: {0}", age);
         }
     }
 
@@ -45,14 +30,8 @@ namespace CyberSpaceAcademy
     {
         public static void Main()
         {
-            DClass d = new DClass();
+            Details d = new Details();
             d.GetInfo();
-
-            BClass b = new BClass();
-            b.GetInfo();
-
-            d.Name = String.Empty;
-            Console.WriteLine(d.Name);
         }
     }
 }
