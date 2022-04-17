e5141c1ff4462f4a04c39006223c2e1464c130cb
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-17 08:13:20 -0700

    Practice on virtual keyword

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -4,17 +4,36 @@ using System;
 namespace CyberSpaceAcademy
 {
 
-    public  class BClass
+    public class BClass 
     {
+
+        public virtual string Name { get; set; }
 
         public virtual void GetInfo()
         {
-            Console.WriteLine("Learn C# Tutorial");
+            Console.WriteLine("Learn C# Tuturial");
         }
     }
 
     public class DClass : BClass
     {
+        private string name;
+
+        public override string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    name = "No Value";
+                }
+            }
+        }
 
         public override void GetInfo()
         {
@@ -24,14 +43,16 @@ namespace CyberSpaceAcademy
 
     public class Program
     {
-
         public static void Main()
         {
             DClass d = new DClass();
             d.GetInfo();
-            
+
             BClass b = new BClass();
             b.GetInfo();
+
+            d.Name = String.Empty;
+            Console.WriteLine(d.Name);
         }
     }
 }
