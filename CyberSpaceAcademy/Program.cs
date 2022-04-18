2268f091f7940103a0aa791cba1e1502d7ff85e3
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-18 15:20:39 -0700

    Practice on delegate

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -3,35 +3,33 @@ using System;
 
 namespace CyberSpaceAcademy
 {
+    public delegate void SampleDelegate(int a, int b);
+    public class MathOperation
+    {
 
-    interface IUser
-    {
-        public void GetDetails(string x);
-    }
-    public class User : IUser
-    {
-        public void GetDetails(string a)
+        public void Add(int a, int b)
         {
-            Console.WriteLine("Name: {0}", a);
+            Console.WriteLine("Add result: {0}", a + b);
         }
-    }
-    public class User1 : IUser
-    {
-        public void GetDetails(string a)
+
+        public void Subtract(int x, int y)
         {
-            Console.WriteLine("Location: {0}", a);
+            Console.WriteLine("Subtract result: {0}", x - y);
+        }
+        public void Multiply(int x, int y)
+        {
+            Console.WriteLine("Multiply result {0}", x * y);
         }
     }
     public class Program
     {
         public static void Main()
         {
-            User u = new User();
-            u.GetDetails("Jimi");
-
-            User1 u1 = new User1();
-            u1.GetDetails("Lagos");
-           
+            MathOperation m = new MathOperation();
+            SampleDelegate dgt = m.Add;
+            dgt += m.Subtract;
+            dgt += m.Multiply;
+            dgt(10, 9);
         }
     }
 }
