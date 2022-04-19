38d16d16496dc32d4266a3385ef3cf12c5f8ff52
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-19 06:28:45 -0700

    Practice on Generic class example

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -3,75 +3,23 @@ using System;
 
 namespace CyberSpaceAcademy
 {
-    public class Maths
+    public class GenericClass<T>
     {
-
-        public delegate void SampleDelegate();
-        public event SampleDelegate SampleEvent;
-        public void Add(int a, int b)
+        public T msg;
+        public void GenericMethod(T name, T location)
         {
-            if (SampleEvent != null)
-            {
-                SampleEvent();
-                Console.WriteLine("Add result: {0}", a + b);
-            }
-            else
-            {
-                Console.WriteLine("Not subscribed to event");
-            }
+            Console.WriteLine("{0}", msg);
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Location: {0}", location);
         }
-
-        public void Subtract(int x, int y)
-        {
-            if(SampleEvent != null)
-            {
-                SampleEvent();
-                Console.WriteLine("subtract result: {0}", x - y);
-            }
-            else
-            {
-                Console.WriteLine("Not subscribed to event");
-            }
-        }
-    }
-    public class Operations
-    {
-
-        Maths m;
-        public int a { get; set; }
-        public int b { get; set; }
-        public Operations(int x, int y)
-        {
-            m = new Maths();
-            m.SampleEvent += SampleEventHandler;
-            a = x;
-            b = y;
-        }
-
-        public void SampleEventHandler()
-        {
-            Console.WriteLine("SampleEvent Handler: Calling Method");
-        }
-
-        public void AddOperation()
-        {
-            m.Add(a, b);
-        }
-
-        public void SubOperation()
-        {
-            m.Subtract(a, b);
-        }
-    }
+    }    
     public class Program
     {
         public static void Main()
         {
-            Console.WriteLine("****Event Example****");
-            Operations op = new Operations(10, 9);
-            op.AddOperation();
-            op.SubOperation();
-            Console.ReadLine(); 
+            GenericClass<string> gclass = new GenericClass<string>();
+            gclass.msg = "Welcome to Tutlane";
+            gclass.GenericMethod("Jimi", "Lagos");
         }
     }
 }
