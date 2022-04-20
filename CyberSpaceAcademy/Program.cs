11639f7ae891c0a3f154d0385f697a51e495df44
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-20 14:23:09 -0700

    Access array element with for loop

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -36,11 +36,10 @@ namespace CyberSpaceAcademy
             //Console.WriteLine(isEqual2);
 
             int[] array = new int[5] {1, 2, 3, 4, 5};
-            Console.WriteLine(array[0]);
-            Console.WriteLine(array[1]);
-            Console.WriteLine(array[2]);
-            Console.WriteLine(array[3]);
-            Console.WriteLine(array[4]);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
