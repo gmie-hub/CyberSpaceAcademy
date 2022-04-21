cba0913cbe8d9e9802df54b47adb4b8ed51c9292
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-21 15:46:38 -0700

    Example on two and three dimensional array

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -6,52 +6,92 @@ namespace ClassWork
     {
         static void Main()
         {
-            Console.WriteLine("This application convert dollar, pound and euro to naira");
-            
-            Console.WriteLine("Enter currency");
-            var currency = Console.ReadLine().ToUpper().Trim();
+            //Console.WriteLine("This application convert dollar, pound and euro to naira");
 
-            Console.WriteLine("\nEnter the amount in digit");
-            var amount =Convert.ToDouble( Console.ReadLine());
-            Console.WriteLine(" ");
-            const double toNaira = 580;
-            string d  = "Dollars";
-            string p = "Pounds";
-            string e = "Euros";
-            string dol = "Dollar";
-            string eu = "Euro";
-            string pd = "pounds";
+            //Console.WriteLine("Enter currency");
+            //var currency = Console.ReadLine().ToUpper().Trim();
 
-            switch (currency)
+            //Console.WriteLine("\nEnter the amount in digit");
+            //var amount =Convert.ToDouble( Console.ReadLine());
+            //Console.WriteLine(" ");
+            //const double toNaira = 580;
+            //string d  = "Dollars";
+            //string p = "Pounds";
+            //string e = "Euros";
+            //string dol = "Dollar";
+            //string eu = "Euro";
+            //string pd = "pounds";
+
+            //switch (currency)
+            //{
+            //    case "DOLLAR":
+            //    case "D":
+            //        if (amount <= 1)
+            //            Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, dol);
+            //        else
+            //            Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, d);
+            //        break;
+
+            //    case "POUND":
+            //    case "P":
+            //        if(amount <= 1)
+            //            Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, pd);
+            //        else
+            //            Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, p);
+            //        break;
+
+            //    case "EURO":
+            //    case "E":
+            //        if( amount <= 1)
+            //            Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, eu);
+            //            Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, e);
+            //        break;
+
+            //    default : Console.WriteLine("Invalid Input");
+            //        break;
+
+            //}
+
+            int[,] array2D = new int[3, 2]
             {
-                case "DOLLAR":
-                case "D":
-                    if (amount <= 1)
-                        Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, dol);
-                    else
-                        Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, d);
-                    break;
+                {4, 5},
+                {5, 0},
+                {3, 1}
+            };
 
-                case "POUND":
-                case "P":
-                    if(amount <= 1)
-                        Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, pd);
-                    else
-                        Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, p);
-                    break;
+            int[,,] array3D = new int[2, 2, 3]
+            {
+                {
+                    {1, 2, 3},
+                    {4, 5, 6}
+                },
 
-                case "EURO":
-                case "E":
-                    if( amount <= 1)
-                        Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, eu);
-                        Console.WriteLine(amount + "{0}" + " = " + amount * toNaira, e);
-                    break;
+                {
+                    {7, 8, 9},
+                    {8, 9, 10}
+                }
+            };
 
-                default : Console.WriteLine("Invalid Input");
-                    break;
-
+            Console.WriteLine("****Two dimensional array elements****");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"a[{i},{j}] = {array2D[i, j]}");
+                }
             }
 
+            Console.WriteLine("****Three dimensional array element");
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Console.WriteLine($"a[{i},{j},{k}] = {array3D[i, j, k]}");
+                    }
+                }
+            }
         }
 
     }
