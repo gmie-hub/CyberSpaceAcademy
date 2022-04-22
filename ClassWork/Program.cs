97fa049a693c1083cb20135124dc442a602c12e7
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-22 02:23:01 -0700

    Pactice on jagged array

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -52,48 +52,52 @@ namespace ClassWork
 
             //}
 
-            int[,] array2D = new int[3, 2]
-            {
-                {4, 5},
-                {5, 0},
-                {3, 1}
-            };
 
-            int[,,] array3D = new int[2, 2, 3]
-            {
-                {
-                    {1, 2, 3},
-                    {4, 5, 6}
-                },
+            int[][] jarray = new int[3][];
+            jarray[0] = new int[5] { 1, 2, 3, 4, 5};
+            jarray[1] = new int[3] { 10, 20, 30 };
+            jarray[2] = new int[] {12, 50, 60, 70, 32};
 
+            Console.WriteLine("***Jagged array with multi-dimensional element***\n");
+            for (int i = 0; i < jarray.Length; i++)
+            {
+                Console.Write("Element[{0}]: ", i);
+                for (int j = 0; j < jarray[i].Length; j++)
                 {
-                    {7, 8, 9},
-                    {8, 9, 10}
+                    Console.Write("{0}{1}", jarray[i][j], j == (jarray[i].Length - 1) ? "" : " ");
                 }
-            };
-
-            Console.WriteLine("****Two dimensional array elements****");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    Console.WriteLine($"a[{i},{j}] = {array2D[i, j]}");
-                }
+                Console.WriteLine();
             }
 
-            Console.WriteLine("****Three dimensional array element");
-            for(int i = 0; i < 2; i++)
+            int[][,] jarray2 = new int[2][,];
+            jarray2[0] = new int[2, 2]
             {
-                for(int j = 0; j < 2; j++)
+                {15, 24},
+                {43, 54}
+            };
+            jarray2[1] = new int[,]
+            {
+                {11, 12},
+                {13, 14},
+                {25, 26}
+            };
+            Console.WriteLine("\nJagged array with multi-dimensional element\n");
+
+            for(int i = 0; i < jarray2.Length; i++)
+            {
+                Console.Write("Element[{0}]: ", i);
+                for (int j = 0; j < jarray2[i].GetLength(0); j++)
                 {
-                    for (int k = 0; k < 3; k++)
+                    Console.Write("{");
+                    for (int k = 0; k < jarray2[i].GetLength(1); k++)
                     {
-                        Console.WriteLine($"a[{i},{j},{k}] = {array3D[i, j, k]}");
+                        Console.Write("{0}{1}", jarray2[i][j, k], k == (jarray2[i].GetLength(1) - 1) ? "" : " ");
                     }
+                    Console.Write("{0}{1}", "}", j < jarray2.GetLength(0) ? ", " : "");
                 }
+                Console.WriteLine();
             }
         }
 
     }
-   
 }
