8fbb889c38f9a16eca8f8d5d3bb23b157675e500
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-24 08:28:42 -0700

    Accessing elements in a list

--- ClassWork/Program.cs
+++ ClassWork/Program.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 
 namespace ClassWork
 {
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
     public class Program
     {
         static void Main()
         {
+
+            List<int> lst = new List<int>() { 1, 8, 45, 70};
+            Console.WriteLine("Element at 0: " + lst[0]);
+            Console.WriteLine("Element at 2: " + lst[2]);
+
+            List<User> user = new List<User>()
+            {
+                new User{Id = 1, Name = "Jimi", Location = "Lagos"},
+                new User{Id = 1, Name = "Sola", Location = "Ogun"},
+                new User{Id = 1, Name = "Daniel", Location = "Rivers"},
+                new User{Id = 1, Name = "Lateef", Location = "Ibadan"}
+            };
+
+            for (int i = 0; i < user.Count; i++)
+                Console.WriteLine("Id: {0}, Name: {1}, Location: {2} ", user[i].Id, user[i].Name, user[i].Location);
+
+            Console.WriteLine();
+
+            foreach(var u in user)
+            {
+                Console.WriteLine("Id: {0}, Name: {1}, Location: {2} ", u.Id, u.Name, u.Location);
+            }
             //Console.WriteLine("This application convert dollar, pound and euro to naira");
 
             //Console.WriteLine("Enter currency");
@@ -52,31 +80,6 @@ namespace ClassWork
             //        break;
 
             //}
-           
-            List<int> lst = new List<int>();
-            lst.Add(1);
-            lst.Add(8);
-            lst.Add(45);
-
-            List<string> lst2 = new List<string>();
-            lst2.Add("Hi");
-            lst2.Add("Welcome");
-            lst2.Add("to");
-            lst2.Add("tutlane");
-
-            Console.WriteLine("List1 element count: " + lst.Count);
-            Console.WriteLine("list1 Capacity: " + lst.Capacity);
-            foreach(int item in lst)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("List2 Count: " + lst2.Count);
-            Console.WriteLine("List2 Capacity: " + lst2.Capacity);
-            foreach (string item in lst2)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
