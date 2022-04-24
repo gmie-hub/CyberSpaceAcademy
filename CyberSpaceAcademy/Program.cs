62af65bb855377e1186e4ea9c1617d128dd2bcc9
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-24 08:03:20 -0700

    Assigment on sorting list using IComparer

--- CyberSpaceAcademy/Program.cs
+++ CyberSpaceAcademy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ClassLibrary.recap;
 using System;
+using System.Collections.Generic;
 namespace CyberSpaceAcademy
 {
    
@@ -7,29 +8,65 @@ namespace CyberSpaceAcademy
     {
         public static void Main()
         {
-            var employees = new Employee[]
+
+            List<Product> prod = new List<Product>()
             {
-                new Employee(){FullName = "Warren Buffet"},
-                new Employee(){FullName = "Bill Gate"},
-                new Employee(){FullName = "Steve Jobs"}
+                new Product(){Name = "Milo", Quantity = 10},
+                 new Product(){Name = "Peak", Quantity = 12},
+                  new Product(){Name = "Sugar", Quantity = 3},
+                   new Product(){Name = "Indomie", Quantity = 60},
+                    new Product(){Name = "Soap", Quantity = 5},
+                     new Product(){Name = "Cream", Quantity = 2},
+                      new Product(){Name = "Shoe", Quantity = 3},
+                       new Product(){Name = "Brush", Quantity = 2},
+                        new Product(){Name = "Biscuit", Quantity = 30},
+                         new Product(){Name = "Perfume", Quantity = 1},
             };
 
-            var employees2 = new Employee[]
+            prod.Sort(new SortProduct());
+            foreach (var p in prod)
             {
-                new Employee(){FullName = "John Piper"},
-                new Employee(){FullName = "Charles Spurgeon"},
-                new Employee(){FullName = "Arthur Pink"}
-            };
-
-            var staffs = new Staffs(employees);
-            var staffs2 = new Staffs(employees2);
-
-            foreach(var item in staffs)
-            {
-                Console.WriteLine(item);
+                Console.WriteLine("{0}\t{1} ", p.Name, p.Quantity);
             }
-
             Console.WriteLine();
+
+            //prod.Sort(new SortNameProduct());
+            //foreach(var p in prod)
+            //{
+            //    Console.WriteLine("{0}\t{1} ", p.Name, p.Quantity);
+            //}
+            
+            
+            
+            
+            
+            
+            
+            
+            //var employees = new Employee[]
+            //{
+            //    new Employee(){FullName = "Warren Buffet"},
+            //    new Employee(){FullName = "Bill Gate"},
+            //    new Employee(){FullName = "Steve Jobs"}
+            //};
+
+            //var employees2 = new Employee[]
+            //{
+            //    new Employee(){FullName = "John Piper"},
+            //    new Employee(){FullName = "Charles Spurgeon"},
+            //    new Employee(){FullName = "Arthur Pink"}
+            //};
+
+            //var staffs = new Staffs(employees);
+            //var staffs2 = new Staffs(employees2);
+
+            //foreach(var item in staffs)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine();
+
         }
     }
 }
